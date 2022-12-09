7249ca627f1e73cc4dfaeffa1a29bc872ec33e14
Rafa Aguilar <rafael.aguilar@glovoapp.com>
2022-12-09 23:13:53 +0100

    fix: wrong sessiong published

--- session-4/README.md
+++ session-4/README.md
@@ -1,8 +1,30 @@
-# Session 6
-Train and use a model using Docker.
+# Session 4
+Implement a Deep Learning App. Train a model to classify text reviews between positive and negative and deploy a web app to predict on user reviews. 
+Then, deploy the model using Google Cloud App Engine.
 
 ## Installation
-You should create a Dockerfile and build the image using `docker build`
- 
+### With Conda
+Create a conda environment by running
+```
+conda create --name aidl-session5 python=3.8
+```
+Then, activate the environment
+```
+conda activate aidl-session5
+```
+and install the dependencies
+```
+pip install -r requirements.txt
+```
+
 ## Running the project
-Once the project is done, you can train it by running the command `docker run <IMAGE_NAME> train`, and predict with the command `docker run <IMAGE_NAME> predict <INPUT_FEATURES>`. Note that you will need to mount some volumes when using `docker run`, otherwise these commands won't work.
+To train the model run:
+```
+python session-5/train.py
+```
+
+To run the app run:
+```
+python session-5/app/main.py
+```
+then, you can test you app by going to http://localhost:8080
--- session-4/theory-example/main.py
+++ session-4/theory-example/main.py
@@ -4,21 +4,63 @@ from flask import Flask, request
 # called `app` in `main.py`.
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+
+@app.route("/hello", methods=["GET"])
 def hello():
-    """ Return a friendly greeting. """
-    return "This is my app inside a Docker container!\n"
+    """ Return a friendly HTTP greeting. """
+    return "Hello AIDL!\n"
 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/sum", methods=["POST"])
 def post_example():
     """ Example of POST method. The input parameters are x and y"""
     x = float(request.form["x"])
     y = float(request.form["y"])
     result = x + y
-    return f"The result is: {result}\n"
+    return f"The result is: {result}"
+
+
+@app.route("/html-example", methods=["GET"])
+def html_example():
+    """ Return an HTML. """
+    return """
+<html>
+    <head>
+        <title>HTML example</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello AIDL!</h1>
+        This is an example of a table in an HTML:
+        <table>
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>50</td>
+            </tr>
+            <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+            </tr>
+        </table>
+    </body>
+</html>
+"""
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host="localhost", port=8080, debug=True)
     
