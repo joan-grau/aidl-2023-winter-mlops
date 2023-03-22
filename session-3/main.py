7b6386cd5d1e8f849afa60c3d398692754ce7643
joan pascual <joanpascualgrau@gmail.com>
2023-03-22 21:27:57 +0100

    Session 3

--- session-3/main.py
+++ session-3/main.py
@@ -18,7 +18,20 @@ def train_single_epoch(model, train_loader, optimizer):
     accs, losses = [], []
     for x, y in train_loader:
         # You will need to do y = y.unsqueeze(1).float() to add an output dimension to the labels and cast to the correct type
-        ...
+        y = y.unsqueeze(1).float() 
+
+        x = x.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(x)
+        loss = F.binary_cross_entropy_with_logits(outputs, y)
+        loss.backward()
+        acc = binary_accuracy(y, outputs)
+
+        optimizer.step()
+
         losses.append(loss.item())
         accs.append(acc.item())
     return np.mean(losses), np.mean(accs)
@@ -29,7 +42,15 @@ def eval_single_epoch(model, val_loader):
     with torch.no_grad():
         model.eval()
         for x, y in val_loader:
-            ...
+            y = y.unsqueeze(1).float()
+
+            x = x.to(device)
+            y = y.to(device)
+
+            outputs = model(x)
+            loss = F.binary_cross_entropy_with_logits(outputs, y)
+            acc = binary_accuracy(y, outputs)
+
             losses.append(loss.item())
             accs.append(acc.item())
     return np.mean(losses), np.mean(accs)
@@ -37,15 +58,17 @@ def eval_single_epoch(model, val_loader):
 
 def train_model(config):
 
-    data_transforms = transforms.Compose([...])
-    train_dataset = ImageFolder...
+    
+    data_transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    train_dataset = ImageFolder("./Data/dataset/cars_vs_flowers/training_set", data_transforms)
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-    test_dataset = ImageFolder...
+    test_dataset = ImageFolder("./Data/dataset/cars_vs_flowers/test_set", data_transforms)
     test_loader = DataLoader(test_dataset, batch_size=config["batch_size"])
 
     my_model = MyModel().to(device)
 
     optimizer = optim.Adam(my_model.parameters(), config["lr"])
+
     for epoch in range(config["epochs"]):
         loss, acc = train_single_epoch(my_model, train_loader, optimizer)
         print(f"Train Epoch {epoch} loss={loss:.2f} acc={acc:.2f}")
--- session-3/model.py
+++ session-3/model.py
@@ -6,7 +6,28 @@ class MyModel(nn.Module):
 
     def __init__(self):
         super().__init__()
-        ...
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(32, 64, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(64, 128, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Linear(25088, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, 1)
+        )
 
     def forward(self, x):
-        ...
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_layers(x)
+        
+        return x
